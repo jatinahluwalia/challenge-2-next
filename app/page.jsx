5da132066193b35771ed633c097590dc0585c2ff
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-07-29 16:58:08 +0530

    desktop design completed

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import './page.css'
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import Step from '@/components/step'
 
 const steps = [
@@ -31,9 +31,9 @@ export default function Home() {
     phone: "",
   })
   const [price, setPrice] = useState({
-    arcade: "$9/mo",
-    pro: "$12/mo",
-    advanced: "$15/mo",
+    arcade: 9,
+    advanced: 12,
+    pro: 15,
     offer: ""
   })
   const [planDetails, setPlanDetails] = useState({
@@ -45,7 +45,8 @@ export default function Home() {
   const [emptyFields, setEmptyFields] = useState([])
   const [error, setError] = useState()
   const [checked, setchecked] = useState("monthly")
-  const checkboxRef = useRef(null)
+  const [addonDetails, setAddonDetails] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleDetailsChange = (e) => {
     setDetails({
@@ -66,9 +67,9 @@ export default function Home() {
     if (e.target.checked) {
       setchecked("yearly")
       setPrice({
-        arcade: "$90/yr",
-        pro: "$120/yr",
-        advanced: "$150/yr",
+        arcade: 90,
+        advanced: 120,
+        pro: 150,
         offer: "2 months free"
       })
       setPlanDetails({
@@ -78,9 +79,9 @@ export default function Home() {
     } else {
       setchecked("monthly")
       setPrice({
-        arcade: "$9/mo",
-        pro: "$12/mo",
-        advanced: "$15/mo",
+        arcade: 9,
+        advanced: 12,
+        pro: 15,
         offer: ""
       })
       setPlanDetails({
@@ -89,8 +90,31 @@ export default function Home() {
       })
     }
   }
+  const handleAddonChange = (e) => {
+    if (e.target.checked) {
+      setAddonDetails(prev => [...prev, {
+        name: e.target.value.split("+")[0],
+        price: parseInt(e.target.value.split("+")[1])
+      }])
+    } else {
+      setAddonDetails(prev => prev.filter(item => item.name !== e.target.value.split("+")[0]))
+    }
+
+    setAddonDetails(prev => {
+      console.log(prev);
+      return prev
+    })
+  }
+
+  const onChangeClick = () => {
+    setActiveIndex(2)
+    setAddonDetails([])
+  }
 
   const handleBackClick = () => {
+    if (activeIndex === 4) {
+      setAddonDetails([])
+    }
     setActiveIndex(activeIndex - 1)
 
   }
@@ -111,6 +135,17 @@ export default function Home() {
         setError("This field is required")
       }
       if (!details.name || !details.email || !details.phone) return
+    }
+
+    if (activeIndex === 3) {
+      setTotal(prev => {
+        let temp = planDetails.price
+        if (!addonDetails.length) return temp
+        addonDetails.forEach(addon => {
+          temp += addon.price
+        })
+        return temp
+      })
     }
     setActiveIndex(activeIndex + 1)
   }
@@ -191,7 +226,7 @@ export default function Home() {
                   />
                   <div className="radio-box-info">
                     <div className="radio-box-name">Arcade</div>
-                    <div className="radio-box-detail">{price.arcade}</div>
+                    <div className="radio-box-detail">{checked === "yearly" ? `$${price.arcade}/yr` : `$${price.arcade}/mo`}</div>
                     {price.offer && <div className='offer'> {price.offer} </div>}
                   </div>
                 </label>
@@ -205,7 +240,7 @@ export default function Home() {
                   />
                   <div className="radio-box-info">
                     <div className="radio-box-name">Advanced</div>
-                    <div className="radio-box-detail">{price.advanced}</div>
+                    <div className="radio-box-detail">{checked === "yearly" ? `$${price.advanced}/yr` : `$${price.advanced}/mo`}</div>
                     {price.offer && <div className='offer'> {price.offer} </div>}
                   </div>
                 </label>
@@ -219,7 +254,7 @@ export default function Home() {
                   />
                   <div className="radio-box-info">
                     <div className="radio-box-name">Pro</div>
-                    <div className="radio-box-detail">{price.pro}</div>
+                    <div className="radio-box-detail">{checked === "yearly" ? `$${price.pro}/yr` : `$${price.pro}/mo`}</div>
                     {price.offer && <div className='offer'> {price.offer} </div>}
                   </div>
                 </label>
@@ -227,8 +262,8 @@ export default function Home() {
               <div className="annual-plan-container">
                 <div className={`monthly ${checked === "monthly" && "checked"}`}>Monthly</div>
                 <label htmlFor="toggle" className='toggle'>
-                  <div className="circle"></div>
-                  <input type="checkbox" name="planDuration" id="toggle" ref={checkboxRef} onChange={handleCheckboxChange} />
+                  <div className="circle" style={checked === "yearly" ? { marginLeft: "20px" } : {}} />
+                  <input type="checkbox" name="planDuration" id="toggle" onChange={handleCheckboxChange} />
                 </label>
                 <div className={`yearly ${checked === "yearly" && "checked"}`}>Yearly</div>
               </div>
@@ -242,7 +277,7 @@ export default function Home() {
             <form className="step-3">
               <div className="checkbox-container">
                 <label className='checkbox-input-box' htmlFor="online-service">
-                  <input type="checkbox" name="addons" id="online-service" />
+                  <input value={checked === "yearly" ? `Online service+10` : `Online service+1`} type="checkbox" name="addons" id="online-service" onChange={handleAddonChange} />
                   <Image
                     src="/assets/images/icon-checkmark.svg"
                     alt='check'
@@ -258,7 +293,7 @@ export default function Home() {
                   </div>
                 </label>
                 <label className='checkbox-input-box' htmlFor="larger-storage">
-                  <input type="checkbox" name="addons" id="larger-storage" />
+                  <input value={checked === "yearly" ? `Larger storage+20` : `Larger storage+2`} type="checkbox" name="addons" id="larger-storage" onChange={handleAddonChange} />
                   <Image
                     src="/assets/images/icon-checkmark.svg"
                     alt='check'
@@ -274,7 +309,7 @@ export default function Home() {
                   </div>
                 </label>
                 <label className='checkbox-input-box' htmlFor="customize">
-                  <input type="checkbox" name="addons" id="customize" />
+                  <input value={checked === "yearly" ? `Customizable profile+20` : `Customizable profile+2`} type="checkbox" name="addons" id="customize" onChange={handleAddonChange} />
                   <Image
                     src="/assets/images/icon-checkmark.svg"
                     alt='check'
@@ -293,18 +328,70 @@ export default function Home() {
             </form>
           </>
         )}
+        {activeIndex === 4 && (
+          <>
+            <h1>Finishing up</h1>
+            <p>Double-check everything looks OK before confirming.</p>
+
+            <div className="summary-container">
+              <div className="summary-plan-info-wrapper">
+                <div className="summary-plan-info">
+                  <div className="summary-plan-name">
+                    {planDetails.plan.charAt(0).toUpperCase() + planDetails.plan.slice(1)} ({planDetails.planDuration.charAt(0).toUpperCase() + planDetails.planDuration.slice(1)})
+                  </div>
+                  <button className="btn change-btn" onClick={onChangeClick}>
+                    Change
+                  </button>
+                </div>
+                <div className="summary-plan-price">
+                  {checked === "yearly" ? `$${planDetails.price}/yr` : `$${planDetails.price}/mo`}
+                </div>
+              </div>
+              {addonDetails.length > 0 && (
+                <div className="addon-container">
+                  {addonDetails.map((addon) => (
+                    <div className="addon" key={addon.name}>
+                      <div className="addon-name">
+                        {addon.name}
+                      </div>
+                      <div className="addon-price">
+                        {checked === "yearly" ? `+$${addon.price}/yr` : `+$${addon.price}/mo`}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="summary-total-container">
+              <div className="summary-total-text">Total (per {planDetails.planDuration.replace("ly", "")})</div>
+              <div className="summary-total">${total}/{planDetails.planDuration === "yearly" ? "yr" : "mo"}</div>
+            </div>
+          </>
+        )}
+        {activeIndex === 5 && (
+          <div className="confirmation-container">
+            <Image
+              src="/assets/images/icon-thank-you.svg"
+              alt='thank you'
+              height={90}
+              width={90}
+            />
+            <h1>Thank you!</h1>
+            <p className='text-center'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loregaming.com</p>
+          </div>
+        )}
         <div className="btn-container">
-          {activeIndex !== 1 ? (
+          {activeIndex !== 1 && activeIndex !== 5 ? (
             <button className='btn back-btn' onClick={handleBackClick}>
               Go back
             </button>
           ) : (
             <div />
           )}
-          {activeIndex !== 5 ? (<button className='btn next-step-btn' onClick={handleNextClick}>
+          {activeIndex <= 4 ? (<button className='btn next-step-btn' onClick={handleNextClick}>
             Next Step
           </button>) : (
-            <button className='btn confirm-btn' onClick={handleSubmit}>
+            activeIndex < 4 && <button className='btn confirm-btn' onClick={handleSubmit}>
               Confirm
             </button>
           )}
